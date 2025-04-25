bcce0cf5c3059288dd9123cd003668c81631a208
ZhukX_im57@top-academy.ru <ZhukX_im57@top-academy.ru>
2025-04-25 22:08:15 +0300

    +++

--- 2D Arrays/main.cpp
+++ 2D Arrays/main.cpp
@@ -9,8 +9,49 @@ void main()
 	const int COLS = 4;
 	int arr[ROWS][COLS] = 
 	{
-		{1,2,3},{4,5,6},{7,8,9}
+		/*{1,2,3},{4,5,6},{7,8,9}*/
 	};
+
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			arr[i][j]=rand()%100;
+		}
+		
+	}
+
+
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			cout << arr[i][j] << "\t";
+		}
+		cout << endl;
+	}
+	cout << endl;
+
+	for (int i = 0; i < ROWS; i++) 
+	{
+		for(int j=0;j<COLS;j++)
+		{
+			for(int k=i;k<ROWS;k++)
+			{
+				for (int l = k ==i? j+1:0; l < COLS;l++)
+				{
+					if (arr[k][l] < arr[i][j])
+					{
+						int buf = arr[i][j];
+						arr[i][j] = arr[k][l];
+						arr[k][l] = buf;
+					}
+				}
+			}
+		}
+	}
+
+
 	for (int i = 0; i < ROWS; i++)
 	{
 		for (int j = 0; j < COLS; j++)
---
+++ Numerics/main.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+using namespace std;
+//#define BYNARY
+
+void main()//шаблон))
+{
+	setlocale(LC_ALL, "");
+	int decimal;
+	cout << "decimal: "; cin >> decimal;
+#ifdef BYNARY
+	const int n = 32; //max возможная разрядность двоичного числа
+	bool binary[n] = {}; //разряды двоичного числа
+	int i= 0;
+	for (; decimal; decimal >>= 1)binary[i++] = decimal &1;
+	
+	for (--i; i >= 0; i--)cout << binary[i];
+	cout << endl;
+#endif
+	const int n = 8;
+	char hex[n] = {};
+	int i = 0;
+	for (; decimal; decimal /= 16)
+	{
+		hex[i] = decimal % 16;
+		hex[i++] += hex[i] < 10 ? 48 : 55;
+	}
+	for (--i; i >= 0; i--)
+	{
+		cout << hex[i];
+		//cout << char(hex[i] < 10 ? hex[i] + 48 : hex[i] + 55);
+	}
+	
+	cout << endl;
+}
